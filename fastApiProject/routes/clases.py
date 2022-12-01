108daed8de5685434591b688d6baeefbfb8a7ab6
タイラー <66812754+Vader-7@users.noreply.github.com>
2022-12-01 12:33:27 -0300

    ...

--- fastApiProject/routes/clases.py
+++ fastApiProject/routes/clases.py
@@ -7,26 +7,26 @@ clase = APIRouter()
 
 @clase.get('/clases')
 async def getClases():
-    return cursor.execute('SELECT asignatura.nombre_asig, id_clas, horario_ini_clas, horario_fin_clas FROM clase JOIN asignatura ').fetchall(), 'Clases obtenidas'
+    return cursor.execute('SELECT id_clas, horario_ini_clas, horario_fin_clas FROM clases').fetchall(), 'Clases obtenidas'
 
 
 @clase.post('/clases')
 async def postClases(clase: Clase):
-    cursor.execute('INSERT INTO clase(id_clas, horario_ini_clas, horario_fin_clas, id_asig) VALUES(:id, :horario_ini, :horario_fin, :id_asig)', clase.dict())
+    cursor.execute('INSERT INTO clases(id_clas, horario_ini_clas, horario_fin_clas, id_asig) VALUES(:id_clas, :horario_ini, :horario_fin, :id_asig)', clase.dict())
     conection.commit()
     return clase.dict(), 'Clase creada'
 
 
 @clase.put('/clases/{id}')
 async def putClases(id: str, clase: Clase):
-    cursor.execute('UPDATE clase SET horario_ini_clas = :horario_ini, horario_fin_clas = :horario_fin, id_asig = :id_asig WHERE id_clas = :id', [clase.horario_ini, clase.horario_fin, clase.id_asig, id])
+    cursor.execute('UPDATE clase SET horario_ini_clas = :horario_ini, horario_fin_clas = :horario_fin, id_asig = :id_asig WHERE id_clas = :id_clas', [clase.horario_ini, clase.horario_fin, clase.id_asig, id_clas])
     conection.commit()
     return clase.dict(), 'Clase actualizada'
 
 
 @clase.delete('/clases/{id}')
 async def deleteClases(id: str):
-    cursor.execute('DELETE FROM clase WHERE id_clas = :id', [id])
+    cursor.execute('DELETE FROM clases WHERE id_clas = :id_clas', [id])
     conection.commit()
     return 'Clase eliminada'
 
