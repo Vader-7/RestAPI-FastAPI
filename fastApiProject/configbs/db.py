d84211184b8bf397c8405d16af6eed886d80cced
タイラー <66812754+Vader-7@users.noreply.github.com>
2022-12-01 12:14:28 -0300

    ...

--- fastApiProject/configbs/db.py
+++ fastApiProject/configbs/db.py
@@ -2,4 +2,4 @@ import oracledb
 
 conection = oracledb.connect(user='API_ARC', password='Kkkk123_123*', dsn='db202201030958_high', config_dir='/Users/ty/opc/wallet_db',
                              wallet_location='/Users/ty/opc/wallet_db', wallet_password='Kkkk123_123*')
-cursor = conection.cursor();
+cursor = conection.cursor()
